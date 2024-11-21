529b9d13cabfdac25315b8b08eada8e27738756a
refrain62 <nao_h13@hotmail.com>
2024-11-21 23:34:27 +0900

    React Query(TanStack Query)を利用しない場合

--- client/src/components/test.tsx
+++ client/src/components/test.tsx
@@ -1,11 +1,37 @@
 import { trpc } from "../utils/trpc";
 
+import { appRouter } from "../../../server";
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { useEffect, useState } from "react";
+
+const client = createTRPCProxyClient<appRouter> ({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000/trpc',
+    }),
+  ],
+});
+
 const Test = () => {
   const hello = trpc.hello.useQuery();
   console.log(hello);
 
+  const [hello2, setHello2] = useState('');
+  useEffect(() => {
+    const getHello2 = async () => {
+      setHello2( await client.hello.query() );
+      console.log(hello2);
+    };
+
+    getHello2();
+  }, []);
+
   return (
-    <div>{hello.data}</div>
+    <>
+      <div>hello: {hello.data}</div>
+      <div>hello2: {hello2}</div>
+    </>
+    
   )
 };
 
