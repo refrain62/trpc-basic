1e1a034522b6fdc4db5a30f950cb5e70bf144859
refrain62 <nao_h13@hotmail.com>
2024-11-21 17:39:17 +0900

    バックエンドの実装

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,29 @@
 import express from 'express';
+import { initTRPC, router } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
 
 const app = express();
 const PORT = 3000;
+
+const t = initTRPC.create();
+
+const appRouter = t.router({
+  hello: t.procedure.query(() => {
+    return 'Hello World';
+  }),
+});
 
 app.get('/', (_req, res) => {
   res.send('hello')
 });
 
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+  })
+);
+
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+
+export type appRouter = typeof appRouter;
